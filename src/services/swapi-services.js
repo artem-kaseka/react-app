835bfaa0f7aa665e8838fba5f165acaaabf4a5d9
Artem <artem.kaseka@gmail.com>
2019-03-22 21:16:59 +0200

    added person-details

--- src/services/swapi-services.js
+++ src/services/swapi-services.js
@@ -42,12 +42,12 @@ export default class SwapiService {
       return this._transformStarship(starship);
     }
   
-    _extractId(item) {
+    _extractId = (item) => {
       const idRegExp = /\/([0-9]*)\/$/;
       return item.url.match(idRegExp)[1];
-    }
+    };
   
-    _transformPlanet(planet) {
+    _transformPlanet = (planet) => {
       return {
         id: this._extractId(planet),
         name: planet.name,
@@ -57,7 +57,7 @@ export default class SwapiService {
       };
     }
   
-    _transformStarship(starship) {
+    _transformStarship = (starship) => {
       return {
         id: this._extractId(starship),
         name: starship.name,
@@ -71,7 +71,7 @@ export default class SwapiService {
       }
     }
   
-    _transformPerson(person) {
+    _transformPerson = (person) => {
       return {
         id: this._extractId(person),
         name: person.name,
